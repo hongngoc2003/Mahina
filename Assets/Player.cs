e570555240e7200474796aa783140b3abf91b50e
hongngoc2003 <nhngoc1506@gmail.com>
2024-10-24 23:47:58 +0700

    add player flip

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -12,6 +12,10 @@ public class Player : MonoBehaviour {
     [SerializeField] private float groundCheckDistance;
     [SerializeField] private Transform wallCheck;
     [SerializeField] private float wallCheckDistance;
+
+    public int facingDir { get; private set; } = 1;
+    private bool facingRight = true;
+
     [SerializeField] private LayerMask whatIsGround;
     #region Components
     public Animator anim {  get; private set; }
@@ -42,11 +46,24 @@ public class Player : MonoBehaviour {
     }
     public void SetVelocity(float _xVelocity, float _yVelocity) { 
         rb.velocity = new Vector2(_xVelocity, _yVelocity);
+        FlipController(_xVelocity);
     }
 
     public bool IsGroundDetected() => Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
     private void OnDrawGizmos() {
         Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
         Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
+    }
+
+    public void Flip() {
+        facingDir = facingDir * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+    public void FlipController(float _x) {
+        if (_x > 0 && !facingRight)
+            Flip();
+        else if(_x < 0 && facingRight)
+            Flip();
     }
 }
